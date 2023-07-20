e575c9cea013ff687ec227cfea839e1a4087bebc
Alberto Ventura <albertoventura.dev@gmail.com>
2023-07-20 19:50:47 -0300

    config db connection

--- basic-project-backend/app.js
+++ basic-project-backend/app.js
@@ -1,7 +1,8 @@
 /* importar as configurações do servidor */
 var app = require('./src/config/server');
-const { PORT } = require('./src/constant/general');
-
+//const { PORT } = require('./src/constant/general');
+require("dotenv").config();
+PORT = process.env.PORT;
 //var userRoutes = require('./src/app/routes/user.routes')(app);
 
 
--- basic-project-backend/src/config/server.js
+++ basic-project-backend/src/config/server.js
@@ -6,8 +6,12 @@ var express = require('express'),
 /* routes */
 const path = require('../constant/path');
 const userRouter = require('../app/routers/user.router');
+
 /* inciar */
 var app = express();
+
+/* import db connection */
+require("../app/database/db");
 
 /* configurar middlwares */
 app.use(bodyParser.urlencoded({extended: true}));
